d2312646141be7f2e9d54d1d5dfa97bcaaa65be4
ashmoreinc <cain.ashmore@gmail.com>
2022-12-03 13:35:58 +0000

    Day 1

---
+++ Day_1/main.py
@@ -0,0 +1,52 @@
+INPUT_FILE = "input.txt"
+
+
+def parse_file() -> list:
+	cals_count = []
+
+	try:
+		with open(INPUT_FILE, "r") as data:
+			current_elf = []
+			# Loop through the data, if there is a blank line, 
+			# add the data collected to the count list (cals_count)
+			# then reset the list
+			# otherwise add to the data (current_elf)
+			for line in data:
+				if line == '\n':
+					cals_count.append(current_elf)
+					current_elf = []
+				else:
+					current_elf.append(int(line))
+
+			# Check if the last elf was handled, the list should be empty if so
+			# Add the current elf if not
+			if len(current_elf) != 0:
+				cals_count.append(current_elf)
+
+	except Exception as e:
+		print("File read error")
+		return None
+
+	return cals_count
+
+def main():
+	elves = parse_file()
+	total_cals = []
+	for elf in elves:
+		count = 0
+		for cals in elf:
+			count += cals
+		total_cals.append(count)
+
+
+	print(f"Elf count: {len(elves)}")
+	print(f"Highest Calories: {max(total_cals)}, held by elf#{total_cals.index(max(total_cals))+1}")
+	print(f"Lowest Calories: {min(total_cals)}, held by elf#{total_cals.index(min(total_cals))+1}")
+
+	total_cals = sorted(total_cals)
+	top_3 = total_cals[-3:]
+	top_3_total = sum(top_3)
+	print(f"The top three calories carriers hold a total of {top_3_total} calories")
+
+if __name__ == "__main__":
+	main()
